bee6dc4681025a6bc433f54a683f200fa0aa00f2
Orlando Castillo <orlandoc1618@gmail.com>
2020-03-06 00:19:40 -0800

    update scripts

--- scripts/setup_macos.sh
+++ scripts/setup_macos.sh
@@ -7,12 +7,12 @@ if ! command -v brew >/dev/null 2>&1; then
   echo "Installing homebrew"
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
-curl https://raw.githubusercontent.com/orlandoc01/dotfiles/osx/Brewfile --output $HOME/.Brewfile
+curl https://raw.githubusercontent.com/orlandoc01/dotfiles/macOS/Brewfile --output $HOME/.Brewfile
 brew bundle
 
 # setup dotfiles
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-git clone -b osx --bare https://github.com/orlandoc01/dotfiles $HOME/.dotfiles
+git clone -b macOS --bare https://github.com/orlandoc01/dotfiles $HOME/.dotfiles
 cd $HOME && dotfiles checkout ./ && dotfiles submodule update --init --recursive
 chmod +x $HOME/.chunkwmrc
 

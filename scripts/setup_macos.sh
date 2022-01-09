9539ef5fcf4a69af4921253502fd43c71e596a73
Orlando Castillo <orlandoc1618@gmail.com>
2022-01-08 17:00:44 -0800

    fix scripts for macOS

--- scripts/setup_macos.sh
+++ scripts/setup_macos.sh
@@ -7,14 +7,14 @@ if ! command -v brew >/dev/null 2>&1; then
   echo "Installing homebrew"
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
-curl https://raw.githubusercontent.com/orlandoc01/dotfiles/macOS/Brewfile --output $HOME/.Brewfile
-brew bundle
+curl https://raw.githubusercontent.com/orlandoc01/dotfiles/macOS/.Brewfile --output $HOME/.Brewfile
+brew bundle --file $HOME/.Brewfile
 
 # setup dotfiles
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 git clone -b macOS --bare https://github.com/orlandoc01/dotfiles $HOME/.dotfiles
-cd $HOME && dotfiles checkout ./ && dotfiles submodule update --init --recursive
-chmod +x $HOME/.chunkwmrc
+cd $HOME && dotfiles checkout && dotfiles submodule update --init --recursive
+chmod +x $HOME/.yabairc
 
 #setup zsh with oh-my-zsh
 sudo chsh -s /usr/local/bin/zsh $(whoami)

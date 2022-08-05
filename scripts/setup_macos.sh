640d98b081cc036714f10256e3c4ac2cf4a0a6b0
Orlando Castillo <orlandoc1618@gmail.com>
2022-08-04 22:43:24 -0700

    move brewfile and update setup script

--- scripts/setup_macos.sh
+++ scripts/setup_macos.sh
@@ -9,14 +9,14 @@ if ! command -v brew >/dev/null 2>&1; then
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
-curl https://raw.githubusercontent.com/orlandoc01/dotfiles/macOS/.Brewfile --output $HOME/.Brewfile
-brew bundle --file $HOME/.Brewfile
+curl https://raw.githubusercontent.com/orlandoc01/dotfiles/macOS/.Brewfile --output $HOME/.config/Brewfile
+brew bundle --file $HOME/.config/Brewfile
 
 # setup dotfiles
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 git clone -b macOS --bare https://github.com/orlandoc01/dotfiles $HOME/.dotfiles
 cd $HOME && dotfiles checkout && dotfiles submodule update --init --recursive
-chmod +x $HOME/.yabairc
+chmod +x $HOME/.config/yabai/yabairc
 
 #setup zsh with oh-my-zsh
 sudo chsh -s /usr/local/bin/zsh $(whoami)

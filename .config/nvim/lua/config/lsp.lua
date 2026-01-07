ce3eff70563da90cefcfd829ab65c451c917b1fc
Orlando Castillo <orlandoc1618@gmail.com>
2026-01-06 22:55:03 -0800

    update nvim lspconfig

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,103 +1,114 @@
 local M = {}
 
 function M.setup()
-  local nvim_lsp = require('lspconfig')
+  -- on_attach: buffer-local keymaps + options
   local on_attach = function(client, bufnr)
-    local function buf_set_option(opt, val)
-        vim.bo[bufnr][opt] = val
-    end
-    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-    -- Mappings
-    local opts = { buffer = bufnr, noremap = true, silent = false }
-
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', function()
-        vim.cmd('tab split')
-        vim.lsp.buf.definition()
-    end, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<leader>t', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-
-    vim.keymap.set('n', '<leader>e', function()
-      vim.diagnostic.open_float({
-          bufnr = 0,
-          scope = "line",
-          border = "rounded",
-          focusable = false,
-          close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-          source = true,
-          prefix = ' ',
+    local function buf_map(mode, lhs, rhs, desc)
+      vim.keymap.set(mode, lhs, rhs, {
+        buffer = bufnr,
+        noremap = true,
+        silent = false,
+        desc = desc,
       })
-    end, opts)
-
-    vim.keymap.set('n', '[d', function()
-        vim.diagnostic.goto_prev({ float = { border = "rounded" } })
-    end, opts)
-
-    vim.keymap.set('n', ']d', function()
-        vim.diagnostic.goto_next({ float = { border = "rounded" } })
-    end, opts)
-
-    vim.keymap.set('n', '<leader>q', function()
-        vim.diagnostic.setloclist({ open = true })
-    end, opts)
-
-    -- Set some keybinds conditional on server capabilities
-    if client.server_capabilities.documentFormattingProvider then
-        vim.keymap.set('n', '<leader>lf', function()
-            vim.lsp.buf.format({ async = true })
-        end, opts)
     end
 
-    if client.server_capabilities.documentRangeFormattingProvider then
-        vim.keymap.set('n', '<leader>lf', function()
-            vim.lsp.buf.format({ async = true })
-      end, opts)
+    -- omnifunc for completion
+    vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+    -- LSP keymaps
+    buf_map("n", "gD", vim.lsp.buf.declaration, "LSP declaration")
+
+    buf_map("n", "gd", function()
+      vim.cmd("tab split")
+      vim.lsp.buf.definition()
+    end, "LSP definition (tab)")
+
+    buf_map("n", "K", vim.lsp.buf.hover, "LSP hover")
+    buf_map("n", "gi", vim.lsp.buf.implementation, "LSP implementation")
+    buf_map("n", "<leader>t", vim.lsp.buf.signature_help, "Signature help")
+    buf_map("n", "<leader>rn", vim.lsp.buf.rename, "Rename symbol")
+    buf_map("n", "gr", vim.lsp.buf.references, "References")
+
+    buf_map("n", "<leader>e", function()
+      vim.diagnostic.open_float(nil, {
+        scope = "line",
+        border = "rounded",
+        focusable = false,
+      })
+    end, "Line diagnostics")
+
+    buf_map("n", "[d", function()
+      vim.diagnostic.goto_prev({ float = { border = "rounded" } })
+    end, "Prev diagnostic")
+
+    buf_map("n", "]d", function()
+      vim.diagnostic.goto_next({ float = { border = "rounded" } })
+    end, "Next diagnostic")
+
+    buf_map("n", "<leader>q", function()
+      vim.diagnostic.setloclist({ open = true })
+    end, "Diagnostics loclist")
+
+    local cap = client.server_capabilities
+
+    if cap.documentFormattingProvider then
+      buf_map("n", "<leader>lf", function()
+        vim.lsp.buf.format({ async = true })
+      end, "Format buffer")
+    end
+
+    if cap.documentRangeFormattingProvider then
+      buf_map("v", "<leader>lf", function()
+        vim.lsp.buf.format({ async = true })
+      end, "Format selection")
+    end
   end
 
-
-  end
-  -- NOTE: Don't use more than 1 servers otherwise nvim is unstable
+  -- Global capabilities (for all servers)
   local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+  capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
   capabilities.textDocument.completion.completionItem.snippetSupport = true
-  -- Use gopls
-  nvim_lsp.gopls.setup{ on_attach = on_attach }
-  nvim_lsp.ts_ls.setup{ on_attach = on_attach }
-  nvim_lsp.rust_analyzer.setup{ on_attach = on_attach }
-  nvim_lsp.clangd.setup{ on_attach = on_attach }
 
-  -- nvim_lsp.solc.setup{ on_attach = on_attach }
-  nvim_lsp.lua_ls.setup {
+  -- Apply on_attach + capabilities to all LSP configs
+  vim.lsp.config("*", {
     on_attach = on_attach,
+    capabilities = capabilities,
+  })
+
+  -- Lua specific configuration
+  vim.lsp.config("lua_ls", {
     settings = {
       Lua = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        runtime = { version = 'LuaJIT' },
-          -- Get the language server to recognize the `vim` global
-        diagnostics = { globals = {'vim'} },
-        -- Make the server aware of Neovim runtime files
+        runtime = { version = "LuaJIT" },
+        diagnostics = { globals = { "vim" } },
         workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-        -- Do not send telemetry data containing a randomized but unique identifier
         telemetry = { enable = false },
       },
     },
-  }
+  })
+
+  -- Enable the LSP servers 
+  vim.lsp.enable({
+    "gopls",
+    "ts_ls",
+    "rust_analyzer",
+    "clangd",
+    "lua_ls",
+  })
+
+  -- Diagnostics signs + UI
   vim.fn.sign_define("DiagnosticSignError", { text = "✗", texthl = "DiagnosticSignError" })
-  vim.fn.sign_define("DiagnosticSignWarn", { text = "!", texthl = "DiagnosticSignWarn" })
+  vim.fn.sign_define("DiagnosticSignWarn",  { text = "!",  texthl = "DiagnosticSignWarn"  })
   vim.fn.sign_define("DiagnosticSignInformation", { text = "", texthl = "DiagnosticSignInfo" })
-  vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
-  -- global config for diagnostic
+  vim.fn.sign_define("DiagnosticSignHint",  { text = "", texthl = "DiagnosticSignHint" })
+
   vim.diagnostic.config({
     underline = false,
     virtual_text = true,
     signs = true,
     severity_sort = true,
   })
-  -- Change border of documentation hover window, See https://github.com/neovim/neovim/pull/13998.
+
   vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
     border = "rounded",
   })
